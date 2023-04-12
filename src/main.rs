cac4d9a543a00ea136bf5b9731a077c96228dab8
Fabian Achammer <fabian@achammer.dev>
2023-04-12 20:27:05 +0200

    can add polynomials

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-#[derive(Debug)]
+use std::ops::Add;
+
+#[derive(Debug, PartialEq, Eq)]
 struct Polynomial(Vec<u32>);
+
+impl Add for Polynomial {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.0.len() > rhs.0.len() {
+            rhs + self
+        } else {
+            Self(
+                self.0
+                    .into_iter()
+                    .chain(std::iter::repeat(0))
+                    .zip(rhs.0.into_iter())
+                    .map(|(x, y)| x + y)
+                    .collect(),
+            )
+        }
+    }
+}
 
 fn main() {
     let p = Polynomial(vec![1, 2, 3]);
-    println!("{p:?}");
+    let q = Polynomial(vec![1, 2]);
+    assert_eq!(p + q, Polynomial(vec![2, 4, 3]));
+    println!("{p:?}", p = Polynomial(vec![1, 2, 3]));
 }
