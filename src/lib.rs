2e8fbe72a2471437aeafa705173bca9212a11017
Fabian Achammer <fabian@achammer.dev>
2023-05-08 21:53:53 +0200

    implement parsing of terms

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod proof_search;
 mod substitution;
 mod term;
 
-use std::str::FromStr;
+use std::{iter::Peekable, str::FromStr};
 
 use disequality::TermDisequality;
 use proof_search::search_proof;
@@ -82,13 +82,16 @@ fn oninput(_event: InputEvent) {
 }
 
 impl FromStr for Term {
-    type Err = InvalidCharacter;
+    type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        TermParser::parse(s)
+        let tokens = tokens(s)?;
+        let term = TermParser::new(tokens).term()?;
+        Ok(term)
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum TermToken {
     Variable { name: char },
     Whitespace,
@@ -100,7 +103,7 @@ enum TermToken {
     Star,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct InvalidCharacter(char);
 
 fn tokens(input: &str) -> Result<impl Iterator<Item = TermToken> + '_, InvalidCharacter> {
@@ -121,18 +124,133 @@ fn tokens(input: &str) -> Result<impl Iterator<Item = TermToken> + '_, InvalidCh
     Ok(tokens.into_iter())
 }
 
-struct TermParser<'a> {
-    input: &'a str,
-    index: usize,
+struct TermParser<I>
+where
+    I: Iterator<Item = TermToken>,
+{
+    tokens: Peekable<I>,
 }
 
-impl<'a> TermParser<'a> {
-    fn parse(input: &'a str) -> Result<Term, InvalidCharacter> {
-        let mut parser = Self { input, index: 0 };
+impl<I> TermParser<I>
+where
+    I: Iterator<Item = TermToken>,
+{
+    fn new(iter: I) -> Self {
+        Self {
+            tokens: iter.peekable(),
+        }
+    }
 
-        let tokens = tokens(input)?;
+    pub fn term(&mut self) -> Result<Term, ParseError> {
+        let term = self.add()?;
+        if self.tokens.next().is_none() {
+            Ok(term)
+        } else {
+            Err(ParseError)
+        }
+    }
 
-        Ok(Term::Zero)
+    fn add(&mut self) -> Result<Term, ParseError> {
+        let mut left = self.mul()?;
+
+        while let Some(TermToken::Plus) = self.tokens.next_if_eq(&TermToken::Plus) {
+            let right = self.mul()?;
+            left = Term::Add(Box::new(left), Box::new(right));
+        }
+
+        Ok(left)
+    }
+
+    fn mul(&mut self) -> Result<Term, ParseError> {
+        let mut left = self.successor()?;
+
+        while let Some(TermToken::Star) = self.tokens.next_if_eq(&TermToken::Star) {
+            let right = self.successor()?;
+            left = Term::Mul(Box::new(left), Box::new(right));
+        }
+
+        Ok(left)
+    }
+
+    fn successor(&mut self) -> Result<Term, ParseError> {
+        match self.tokens.next_if_eq(&TermToken::Successor) {
+            Some(TermToken::Successor) => Ok(Term::S(Box::new(self.successor()?))),
+            _ => self.zero_or_variable(),
+        }
+    }
+
+    fn zero_or_variable(&mut self) -> Result<Term, ParseError> {
+        match self.tokens.next() {
+            Some(TermToken::Zero) => Ok(Term::Zero),
+            Some(TermToken::Variable { name }) => Ok(Term::Variable(name.into())),
+            Some(TermToken::LeftParenthesis) => {
+                let term = self.add()?;
+                if let Some(TermToken::RightParenthesis) = self.tokens.next() {
+                    Ok(term)
+                } else {
+                    Err(ParseError)
+                }
+            }
+            _ => Err(ParseError),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    TokenizerError(InvalidCharacter),
+    ParserError(ParseError),
+}
+
+impl From<InvalidCharacter> for Error {
+    fn from(i: InvalidCharacter) -> Self {
+        Self::TokenizerError(i)
+    }
+}
+
+impl From<ParseError> for Error {
+    fn from(p: ParseError) -> Self {
+        Self::ParserError(p)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseError;
+
+#[cfg(test)]
+mod test {
+    use crate::term::Term;
+
+    #[test]
+    fn parse_test() {
+        assert_eq!(
+            "SS0*y+S0".parse(),
+            Ok(Term::Add(
+                Term::Mul(
+                    Term::S(Term::S(Term::Zero.into()).into()).into(),
+                    Term::Variable('y'.into()).into()
+                )
+                .into(),
+                Term::S(Term::Zero.into()).into()
+            ))
+        )
+    }
+
+    #[test]
+    fn parse_test_with_parenthesis() {
+        assert_eq!(
+            "SS(0*y+S0)".parse(),
+            Ok(Term::S(
+                Term::S(
+                    Term::Add(
+                        Term::Mul(Term::Zero.into(), Term::Variable('y'.into()).into()).into(),
+                        Term::S(Term::Zero.into()).into()
+                    )
+                    .into()
+                )
+                .into()
+            ))
+        )
     }
 }
 
