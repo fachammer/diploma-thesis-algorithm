5220995d10fb1e1b13c276e44abbb0c62f03a0a8
Fabian Achammer <fabian@achammer.dev>
2023-05-08 07:34:40 +0200

    implement callbacks for input fields

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,31 @@ use disequality::TermDisequality;
 use proof_search::search_proof;
 use term::Term;
 use wasm_bindgen::prelude::*;
+use web_sys::{console, HtmlElement};
 
 #[wasm_bindgen(start)]
-pub fn greet() {
-    let s = "Hello, world";
-    web_sys::console::log_1(&JsValue::from_str(s));
+pub fn main() {
+    let window = web_sys::window().expect("window must exist");
+    let document = window.document().expect("document must exist");
+    let left_input: HtmlElement = document
+        .get_element_by_id("left-term")
+        .expect("left-term must exist")
+        .unchecked_into();
+    let right_input: HtmlElement = document
+        .get_element_by_id("right-term")
+        .expect("right-term must exist")
+        .unchecked_into();
+    let left_input_on_change: Closure<dyn FnMut()> =
+        Closure::wrap(Box::new(|| console::log_1(&"on left-input change".into())));
+    left_input.set_oninput(Some(left_input_on_change.as_ref().unchecked_ref()));
+    left_input_on_change.forget();
+
+    let right_input_on_change: Closure<dyn FnMut()> =
+        Closure::wrap(Box::new(|| console::log_1(&"on right-input change".into())));
+    right_input.set_oninput(Some(right_input_on_change.as_ref().unchecked_ref()));
+    right_input_on_change.forget();
+
+    web_sys::console::log_1(&"main ended".into());
 }
 
 #[wasm_bindgen]
