c68ce396c358751378c5c1b107de78d25daff22c
Fabian Achammer <fabian@achammer.dev>
2023-05-09 07:04:29 +0200

    implement basic display of term in html

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use disequality::TermDisequality;
 use proof_search::search_proof;
 use term::Term;
 use wasm_bindgen::prelude::*;
-use web_sys::{console, Document, HtmlInputElement, InputEvent};
+use web_sys::{console, Document, Element, HtmlElement, HtmlInputElement, InputEvent, Node, Text};
 
 fn unchecked_document() -> Document {
     let window = web_sys::window().expect("window must exist");
@@ -20,6 +20,10 @@ fn unchecked_document() -> Document {
 }
 
 fn unchecked_input_by_id(id: &str) -> HtmlInputElement {
+    unchecked_element_by_id(id).unchecked_into()
+}
+
+fn unchecked_element_by_id(id: &str) -> web_sys::Element {
     unchecked_document()
         .get_element_by_id(id)
         .unwrap_or_else(|| {
@@ -31,7 +35,6 @@ fn unchecked_input_by_id(id: &str) -> HtmlInputElement {
                     .to_string()
             )
         })
-        .unchecked_into()
 }
 
 fn left_input() -> HtmlInputElement {
@@ -42,10 +45,22 @@ fn right_input() -> HtmlInputElement {
     unchecked_input_by_id("right-term")
 }
 
+fn left_term_display() -> HtmlElement {
+    unchecked_element_by_id("left-term-display").unchecked_into()
+}
+
+fn right_term_display() -> HtmlElement {
+    unchecked_element_by_id("right-term-display").unchecked_into()
+}
+
 fn oninput(_event: InputEvent) {
     let left_value = left_input().value();
     let right_value = right_input().value();
 
+    display(left_value, right_value);
+}
+
+fn display(left_value: String, right_value: String) {
     console::log_4(
         &"as strings: ".into(),
         &left_value.as_str().into(),
@@ -65,6 +80,17 @@ fn oninput(_event: InputEvent) {
         &"≠".into(),
         &serde_wasm_bindgen::to_value(&right).unwrap(),
     );
+
+    let left_display: Element = left_term_display().into();
+    left_display.set_text_content(None);
+    left_display
+        .append_child(&Node::from(left.clone()))
+        .expect("append child must work");
+    let right_display = right_term_display();
+    right_display.set_text_content(None);
+    right_display
+        .append_child(&Node::from(right.clone()))
+        .expect("append child must work");
 
     let disequality = TermDisequality::from_terms(left, right);
 
@@ -217,6 +243,95 @@ impl From<ParseError> for Error {
 #[derive(Debug, PartialEq, Eq)]
 pub struct ParseError;
 
+impl From<Term> for Node {
+    fn from(term: Term) -> Self {
+        match term {
+            Term::Variable(x) => unchecked_document()
+                .create_text_node(&format!(
+                    "{}",
+                    char::try_from(x).expect("must be a valid char value")
+                ))
+                .into(),
+            Term::Zero => unchecked_document().create_text_node("0").into(),
+            Term::S(inner) => {
+                let inner_node = Node::from(*inner);
+                let node = unchecked_document()
+                    .create_element("span")
+                    .expect("create element must work");
+
+                node.append_child(&unchecked_document().create_text_node("S").into())
+                    .expect("append child must work");
+                node.append_child(&inner_node)
+                    .expect("append child must work");
+
+                node.into()
+            }
+            Term::Add(left, right) => {
+                let left_node = Node::from(*left);
+                let right_node = Node::from(*right);
+                let node = unchecked_document()
+                    .create_element("span")
+                    .expect("create element must work");
+
+                node.append_child(&left_node)
+                    .expect("append child must work");
+                node.append_child(&unchecked_document().create_text_node("+").into())
+                    .expect("append child must work");
+                node.append_child(&right_node)
+                    .expect("append child must work");
+
+                node.into()
+            }
+            Term::Mul(left, right) => {
+                let left_node = Node::from(*left);
+                let right_node = Node::from(*right);
+                let node = unchecked_document()
+                    .create_element("span")
+                    .expect("create element must work");
+
+                node.append_child(&left_node)
+                    .expect("append child must work");
+                node.append_child(&unchecked_document().create_text_node("*").into())
+                    .expect("append child must work");
+                node.append_child(&right_node)
+                    .expect("append child must work");
+
+                node.into()
+            }
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    let left_input = left_input();
+    let left_input_on_change: Closure<dyn Fn(InputEvent)> = Closure::wrap(Box::new(oninput));
+    left_input.set_oninput(Some(left_input_on_change.as_ref().unchecked_ref()));
+    left_input_on_change.forget();
+
+    let right_input = right_input();
+    let right_input_on_change: Closure<dyn Fn(InputEvent)> = Closure::wrap(Box::new(oninput));
+    right_input.set_oninput(Some(right_input_on_change.as_ref().unchecked_ref()));
+    right_input_on_change.forget();
+
+    display(left_input.value(), right_input.value());
+
+    console::log_1(&"main ended".into());
+}
+
+#[wasm_bindgen]
+pub fn search() -> Result<JsValue, JsValue> {
+    match search_proof(&TermDisequality::from_terms(
+        Term::Mul(Term::S(Term::Zero.into()).into(), Term::Variable(0).into()),
+        Term::S(Term::Zero.into()),
+    )) {
+        Ok(proof) => Ok(serde_wasm_bindgen::to_value(&proof)?),
+        Err(attempt) => Err(serde_wasm_bindgen::to_value(&attempt)?),
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::term::Term;
@@ -251,33 +366,5 @@ mod test {
                 .into()
             ))
         )
-    }
-}
-
-#[wasm_bindgen(start)]
-pub fn main() {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-
-    let left_input = left_input();
-    let left_input_on_change: Closure<dyn Fn(InputEvent)> = Closure::wrap(Box::new(oninput));
-    left_input.set_oninput(Some(left_input_on_change.as_ref().unchecked_ref()));
-    left_input_on_change.forget();
-
-    let right_input = right_input();
-    let right_input_on_change: Closure<dyn Fn(InputEvent)> = Closure::wrap(Box::new(oninput));
-    right_input.set_oninput(Some(right_input_on_change.as_ref().unchecked_ref()));
-    right_input_on_change.forget();
-
-    console::log_1(&"main ended".into());
-}
-
-#[wasm_bindgen]
-pub fn search() -> Result<JsValue, JsValue> {
-    match search_proof(&TermDisequality::from_terms(
-        Term::Mul(Term::S(Term::Zero.into()).into(), Term::Variable(0).into()),
-        Term::S(Term::Zero.into()),
-    )) {
-        Ok(proof) => Ok(serde_wasm_bindgen::to_value(&proof)?),
-        Err(attempt) => Err(serde_wasm_bindgen::to_value(&attempt)?),
     }
 }
