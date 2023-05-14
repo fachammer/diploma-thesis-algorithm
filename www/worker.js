9308e7fa1663746fab09f1e5ff36f95a987ce565
Fabian Achammer <fabian@achammer.dev>
2023-05-14 10:45:33 +0200

    simplify init code

--- www/worker.js
+++ www/worker.js
@@ -3,8 +3,7 @@ importScripts('../pkg/thesis_algorithm.js');
 async function init() {
     await wasm_bindgen('../pkg/thesis_algorithm_bg.wasm');
 
-    const { main_worker } = wasm_bindgen;
-    main_worker();
+    wasm_bindgen.main_worker();
 }
 
 init();
