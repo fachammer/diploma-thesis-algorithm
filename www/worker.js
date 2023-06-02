1570a2c5c10eaf5985d79660b74657d4a4790517
Fabian Achammer <fabian@achammer.dev>
2023-06-02 14:51:59 +0200

    put pkg into www directory

--- www/worker.js
+++ www/worker.js
@@ -1,6 +1,6 @@
-importScripts("../pkg/thesis_algorithm.js");
+importScripts("pkg/thesis_algorithm.js");
 
 self.onmessage = async (event) => {
-  await wasm_bindgen("../pkg/thesis_algorithm_bg.wasm", event.data);
+  await wasm_bindgen("pkg/thesis_algorithm_bg.wasm", event.data);
   wasm_bindgen.main_worker();
 };
