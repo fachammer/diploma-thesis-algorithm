941c0b09e872b321991455daee9db3385c654757
Fabian Achammer <fabian@achammer.dev>
2023-06-16 17:50:39 +0200

    let wasm init do fetch

--- www/worker.js
+++ www/worker.js
@@ -1,7 +1,6 @@
 import init from "./pkg/thesis_algorithm.js";
 
 self.onmessage = async (event) => {
-  const mod = await fetch("./pkg/thesis_algorithm_bg.wasm");
-  const wasm_module = await init(mod, event.data);
+  const wasm_module = await init("./pkg/thesis_algorithm_bg.wasm", event.data);
   wasm_module.main_worker();
 };
