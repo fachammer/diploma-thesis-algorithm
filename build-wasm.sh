a48d95be2fa5135868bf6f5196a4f1263adfeafb
Fabian Achammer <fabian@achammer.dev>
2023-06-17 22:16:46 +0200

    configure memory and stack size

--- build-wasm.sh
+++ build-wasm.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory' wasm-pack build --out-dir www/pkg --profiling --target web -- . -Z 'build-std=std,panic_abort'
+wasm-pack build --no-typescript --out-dir www/pkg --profiling --target web -- . -Z 'build-std=std,panic_abort'
