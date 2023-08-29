fc3602a839f738ccdb3c5fdecbd915336cc7d8fe
Fabian Achammer <fabian@achammer.dev>
2023-08-29 15:25:10 +0200

    implement nix build

--- flake.nix
+++ flake.nix
@@ -3,11 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgsUnstable.url = "github:nixos/nixpkgs/master";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, nixpkgsUnstable, rust-overlay, ... }:
     let
-      mapPerSystemPkgs = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+      mapPerSystemPkgs = f: builtins.mapAttrs
+        (system: pkgs:
+          let
+            overlayedPkgs = import nixpkgs
+              {
+                inherit system;
+                overlays = [
+                  (import rust-overlay)
+                  (final: prev: {
+                    wasm-bindgen-cli = nixpkgsUnstable.legacyPackages.${system}.wasm-bindgen-cli;
+                  })
+                ];
+              };
+          in
+          f system overlayedPkgs)
+        nixpkgs.legacyPackages;
     in
     {
       devShells = mapPerSystemPkgs
@@ -16,8 +33,9 @@
             buildInputs = with pkgs; [
               nil
               nixpkgs-fmt
-              cowsay
             ];
+
+            inputsFrom = [ self.packages.${system}.default ];
           };
         });
 
@@ -26,7 +44,33 @@
 
       packages = mapPerSystemPkgs
         (system: pkgs: {
-          default = { };
+          default =
+            let
+              rust-binary = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+              rustPlatform = pkgs.makeRustPlatform {
+                cargo = rust-binary;
+                rustc = rust-binary;
+              };
+            in
+            rustPlatform.buildRustPackage {
+              name = "diploma-thesis-algorithm";
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+              nativeBuildInputs = with pkgs; [
+                wasm-bindgen-cli
+              ];
+              buildPhase = ''
+                set -ex
+                cargo build --target wasm32-unknown-unknown --release
+                mkdir -p ./www/pkg
+                wasm-bindgen --target web --out-dir ./www/pkg --no-typescript ./target/wasm32-unknown-unknown/release/thesis_algorithm.wasm
+              '';
+              installPhase = ''
+                cp -R ./www/. $out
+              '';
+            };
         });
     };
 }
