f0663dc39579bb5c293cd187a1b07758c5144558
Fabian Achammer <fabian@achammer.dev>
2023-08-30 15:06:45 +0200

    add nixos module and basic vm configuration

--- flake.nix
+++ flake.nix
@@ -71,6 +71,96 @@
                 cp -R ./www/. $out
               '';
             };
+
+          vm-aarch64-linux = (self.nixosConfigurations.default {
+            guestSystem = "aarch64-linux";
+            hostSystem = system;
+          }).config.system.build.vm;
         });
+
+      nixosModules.default = { config, pkgs, lib, ... }:
+        with lib;
+        let cfg = config.services.diploma-thesis-algorithm; in {
+          options = {
+            services.diploma-thesis-algorithm = {
+              enable = mkEnableOption "Enables the diploma-thesis-algorithm service";
+
+              port = mkOption {
+                type = types.port;
+                default = 80;
+              };
+
+              address = mkOption {
+                type = types.str;
+                default = "0.0.0.0";
+              };
+
+              package = mkOption {
+                type = types.package;
+                default = self.packages.${pkgs.system}.default;
+              };
+            };
+          };
+
+          config = mkIf cfg.enable {
+            services.nginx = {
+              enable = true;
+              appendHttpConfig = ''
+                error_log stderr;
+                access_log syslog:server=unix:/dev/log combined;
+              '';
+              virtualHosts = {
+                "${cfg.address}" = {
+                  listen = [{
+                    inherit (cfg) port;
+                    addr = cfg.address;
+                  }];
+                  locations = {
+                    "/".root = "${cfg.package}";
+                  };
+                };
+              };
+            };
+          };
+        };
+
+      nixosConfigurations.default = { hostSystem, guestSystem }: nixpkgs.lib.nixosSystem {
+        system = guestSystem;
+
+        modules = [
+          ({ config, modulesPath, ... }: {
+            imports = [
+              "${modulesPath}/virtualisation/qemu-vm.nix"
+              self.nixosModules.default
+            ];
+            system.stateVersion = "23.05";
+            nixpkgs.localSystem.system = guestSystem;
+            virtualisation.diskImage = "./diploma-thesis-algorithm.qcow2";
+            virtualisation.graphics = false;
+            virtualisation.host.pkgs = nixpkgs.legacyPackages.${hostSystem};
+            virtualisation.forwardPorts = [
+              {
+                from = "host";
+                host.port = 8080;
+                guest.port = config.services.diploma-thesis-algorithm.port;
+              }
+            ];
+
+            networking.firewall = {
+              enable = true;
+              allowedTCPPorts = [
+                config.services.diploma-thesis-algorithm.port
+              ];
+            };
+
+            services.getty.autologinUser = "root";
+
+            services.diploma-thesis-algorithm = {
+              enable = true;
+              port = 80;
+            };
+          })
+        ];
+      };
     };
 }
