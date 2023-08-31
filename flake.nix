b917d1c58a7a997f7fab05e11facf8034c0bdb77
Fabian Achammer <fabian@achammer.dev>
2023-08-31 09:29:13 +0200

    use flake-parts to handle per system configuration

--- flake.nix
+++ flake.nix
@@ -3,172 +3,168 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgsUnstable.url = "github:nixos/nixpkgs/master";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, nixpkgsUnstable, rust-overlay, ... }:
-    let
-      mapPerSystemPkgs = f: builtins.mapAttrs
-        (system: pkgs:
-          let
-            overlayedPkgs = import nixpkgs
-              {
-                inherit system;
-                overlays = [
-                  (import rust-overlay)
-                  (final: prev: {
-                    wasm-bindgen-cli = nixpkgsUnstable.legacyPackages.${system}.wasm-bindgen-cli;
-                  })
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, rust-overlay, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
+        nixosModules.default = { config, pkgs, lib, ... }:
+          with lib;
+          let cfg = config.services.diploma-thesis-algorithm;
+          in {
+            options = {
+              services.diploma-thesis-algorithm = {
+                enable = mkEnableOption "Enables the diploma-thesis-algorithm service";
+
+                port = mkOption {
+                  type = types.port;
+                  default = 80;
+                };
+
+                address = mkOption {
+                  type = types.str;
+                  default = "0.0.0.0";
+                };
+
+                files = mkOption {
+                  type = types.package;
+                  default = self.packages.${pkgs.system}.files;
+                };
+              };
+            };
+
+            config = mkIf cfg.enable {
+              services.nginx = {
+                enable = true;
+                appendHttpConfig = ''
+                  error_log stderr;
+                  access_log syslog:server=unix:/dev/log combined;
+                '';
+                virtualHosts = {
+                  "${cfg.address}" = {
+                    listen = [{
+                      inherit (cfg) port;
+                      addr = cfg.address;
+                    }];
+                    locations = {
+                      "/".root = "${cfg.files}";
+                    };
+                  };
+                };
+              };
+
+              networking.firewall = {
+                enable = true;
+                allowedTCPPorts = [
+                  config.services.diploma-thesis-algorithm.port
                 ];
               };
-          in
-          f system overlayedPkgs)
-        nixpkgs.legacyPackages;
-    in
-    {
-      devShells = mapPerSystemPkgs
-        (system: pkgs: {
+            };
+          };
+      };
+      systems = [ "aarch64-linux" "aarch64-darwin" ];
+      perSystem = { pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+            (final: prev: {
+              wasm-bindgen-cli = inputs.nixpkgs-unstable.legacyPackages.${system}.wasm-bindgen-cli;
+            })
+          ];
+        };
+
+        devShells = {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
               nil
               nixpkgs-fmt
             ];
 
-            inputsFrom = [ self.packages.${system}.default ];
-          };
-        });
-
-      formatter = mapPerSystemPkgs
-        (system: pkgs: pkgs.nixpkgs-fmt);
-
-      packages = mapPerSystemPkgs
-        (system: pkgs: {
-          default =
-            let
-              rust-binary = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-              rustPlatform = pkgs.makeRustPlatform {
-                cargo = rust-binary;
-                rustc = rust-binary;
-              };
-            in
-            rustPlatform.buildRustPackage {
-              name = "diploma-thesis-algorithm";
-              src = ./.;
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-              };
-              nativeBuildInputs = with pkgs; [
-                wasm-bindgen-cli
-              ];
-              buildPhase = ''
-                set -ex
-                cargo build --target wasm32-unknown-unknown --release
-                mkdir -p ./www/pkg
-                wasm-bindgen --target web --out-dir ./www/pkg --no-typescript ./target/wasm32-unknown-unknown/release/thesis_algorithm.wasm
-              '';
-              installPhase = ''
-                cp -R ./www/. $out
-              '';
-            };
-
-          vm-aarch64-linux = (self.nixosConfigurations.vm {
-            guestSystem = "aarch64-linux";
-            hostSystem = system;
-          }).config.system.build.vm;
-        });
-
-      nixosModules.default = { config, pkgs, lib, ... }:
-        with lib;
-        let cfg = config.services.diploma-thesis-algorithm; in {
-          options = {
-            services.diploma-thesis-algorithm = {
-              enable = mkEnableOption "Enables the diploma-thesis-algorithm service";
-
-              port = mkOption {
-                type = types.port;
-                default = 80;
-              };
-
-              address = mkOption {
-                type = types.str;
-                default = "0.0.0.0";
-              };
-
-              package = mkOption {
-                type = types.package;
-                default = self.packages.${pkgs.system}.default;
-              };
-            };
-          };
-
-          config = mkIf cfg.enable {
-            services.nginx = {
-              enable = true;
-              appendHttpConfig = ''
-                error_log stderr;
-                access_log syslog:server=unix:/dev/log combined;
-              '';
-              virtualHosts = {
-                "${cfg.address}" = {
-                  listen = [{
-                    inherit (cfg) port;
-                    addr = cfg.address;
-                  }];
-                  locations = {
-                    "/".root = "${cfg.package}";
-                  };
-                };
-              };
-            };
-
-            networking.firewall = {
-              enable = true;
-              allowedTCPPorts = [
-                config.services.diploma-thesis-algorithm.port
-              ];
-            };
+            inputsFrom = [ self.packages.${system}.files ];
           };
         };
 
-      nixosConfigurations.vm = { hostSystem, guestSystem }: nixpkgs.lib.nixosSystem {
-        system = guestSystem;
+        formatter = pkgs.nixpkgs-fmt;
 
-        modules = [
-          ({ config, modulesPath, ... }: {
-            imports = [
-              "${modulesPath}/virtualisation/qemu-vm.nix"
-              self.nixosModules.default
-            ];
-            system.stateVersion = "23.05";
-            nixpkgs.localSystem.system = guestSystem;
+        packages =
+          let
+            systemArchitecture = builtins.head (builtins.split "-" system);
+            guestSystem = "${systemArchitecture}-linux";
+            vmNixosSystem =
+              nixpkgs.lib.nixosSystem {
+                system = guestSystem;
 
-            virtualisation.diskImage = "./diploma-thesis-algorithm.qcow2";
-            virtualisation.graphics = false;
-            virtualisation.host.pkgs = nixpkgs.legacyPackages.${hostSystem};
-            virtualisation.forwardPorts = [
-              {
-                from = "host";
-                host.port = 8080;
-                guest.port = config.services.diploma-thesis-algorithm.port;
-              }
-            ];
+                modules = [
+                  ({ config, modulesPath, ... }: {
+                    imports = [
+                      "${modulesPath}/virtualisation/qemu-vm.nix"
+                      self.nixosModules.default
+                    ];
+                    system.stateVersion = "23.05";
+                    nixpkgs.localSystem.system = guestSystem;
 
-            services.getty.autologinUser = "root";
+                    virtualisation.diskImage = "./diploma-thesis-algorithm.qcow2";
+                    virtualisation.graphics = false;
+                    virtualisation.host.pkgs = nixpkgs.legacyPackages.${system};
+                    virtualisation.forwardPorts = [
+                      {
+                        from = "host";
+                        host.port = 8080;
+                        guest.port = config.services.diploma-thesis-algorithm.port;
+                      }
+                    ];
+                    services.getty.autologinUser = "root";
 
-            networking.firewall = {
-              enable = true;
-              allowedTCPPorts = [
-                config.services.diploma-thesis-algorithm.port
-              ];
-            };
+                    services.diploma-thesis-algorithm = {
+                      enable = true;
+                      port = 80;
+                    };
+                  })
+                ];
+              };
+            vm = vmNixosSystem.config.system.build.vm;
+          in
+          {
+            files =
+              let
+                rustBinary = pkgs.rust-bin.stable."1.69.0".default.override {
+                  targets = [ "wasm32-unknown-unknown" ];
+                };
+                rustPlatform = pkgs.makeRustPlatform {
+                  cargo = rustBinary;
+                  rustc = rustBinary;
+                };
+              in
+              rustPlatform.buildRustPackage {
+                name = "diploma-thesis-algorithm";
+                src = ./.;
+                cargoLock = {
+                  lockFile = ./Cargo.lock;
+                };
+                nativeBuildInputs = with pkgs; [
+                  wasm-bindgen-cli
+                ];
+                buildPhase = ''
+                  set -ex
+                  cargo build --target wasm32-unknown-unknown --release
+                  mkdir -p ./www/pkg
+                  wasm-bindgen --target web --out-dir ./www/pkg --no-typescript ./target/wasm32-unknown-unknown/release/thesis_algorithm.wasm
+                '';
+                installPhase = ''
+                  cp -R ./www/. $out
+                '';
+              };
+            inherit vm;
 
-            services.diploma-thesis-algorithm = {
-              enable = true;
-              port = 80;
-            };
-          })
-        ];
+            default = vm;
+          };
       };
     };
 }
